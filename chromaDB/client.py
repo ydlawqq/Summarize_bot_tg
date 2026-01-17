2bf88f25c9ba0af524d72355e5fd1e548956e340
Daniil <volkodot@mail.ru>
2026-01-17 17:46:53 +0300

    Чистка

--- chromaDB/client.py
+++
@@ -1,9 +0,0 @@
-import chromadb
-
-client = chromadb.Client()
-
-collection = client.create_collection("documents")
-
-
-def addDocs(embeddings, chunks, metadata, ids):
-    collection.add(embeddings=[embeddings], documents=[chunks], metadatas=[metadata], ids=[ids])
